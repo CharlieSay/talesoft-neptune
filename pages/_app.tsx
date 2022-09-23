d4ab3d86977fbe8e6b64a7207786fffe652255da
Charlie Say <charliewsay@gmail.com>
2022-09-23 14:46:36 +0100

    debt: Updating SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,10 +7,13 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Head>
-        <meta name="title" content="Talesoft Digital" />
+        <meta
+          name="title"
+          content="Talesoft Digital | Digital Agency based in Manchester"
+        />
         <meta
           name="description"
-          content="Talesoft Digital, custom made high quality websites & software consultation."
+          content=" Creativity, we are for it - Talesoft is a one man digital agency making custom high quality websites & provide software consultation for small - medium size businesses. Based out of Manchester, UK."
         />
         <meta
           name="keywords"

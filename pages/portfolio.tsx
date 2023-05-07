67b23ec9f21d18fd7138c537e01b0d52090a99c0
Charlie Say <charliewsay@gmail.com>
2023-05-07 18:50:10 +0100

    feat: reskin2

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,7 +1,18 @@
 import type { NextPage } from 'next'
 
 const Portfolio: NextPage = () => {
-  return <div className="my-auto">Portfolio</div>
+  return (
+    <div className="my-auto">
+      {' '}
+      <h1 className="uppercase animate-text bg-gradient-to-r from-emerald-500 via-teal-500 to-cyan-500 bg-clip-text text-transparent text-1xl md:text-4xl font-bold">
+        Clients
+      </h1>
+      <p>!Audacious Conferencing</p>
+      <p>!Audacious Church</p>
+      <p>Chandler Watson</p>
+      <p>Bridge Church Woodford</p>
+    </div>
+  )
 }
 
 export default Portfolio

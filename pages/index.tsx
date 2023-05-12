ce5753ca88adb8b5cfdff9d41908a06e44734aa9
Charlie Say <charliewsay@gmail.com>
2023-05-12 15:50:47 +0100

    Gave up used rough-notation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,29 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
+import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 
 const Home: NextPage = () => {
   return (
     <section className="px-4 mt-32 md:mt-64">
-      <h1 className="z-50 mb-7 antialiased text-4xl md:text-5xl font-semibold">
+      <h1 className="z-50 mb-7 antialiased text-2xl md:text-5xl font-semibold">
         Amplify your message through
       </h1>
-      <span className="highlight-container">
-        <span className="pt-4 text-4xl md:text-5xl font-extrabold uppercase highlight">
-          <h1 className="invertMe">Digital Transformation</h1>
-        </span>
-      </span>
+      <h1 className="hidden md:block pt-4 text-5xl font-extrabold uppercase">
+        <RoughNotation
+          type="circle"
+          color="#1d4fd8"
+          padding={[20, 100]}
+          show={true}
+        >
+          Digital Transformation
+        </RoughNotation>
+      </h1>
+      <h1 className="block text-white flex-wrap md:hidden pt-4 text-5xl font-extrabold uppercase">
+        <RoughNotation type="highlight" color="#1d4fd8" show={true}>
+          Digital Transformation
+        </RoughNotation>
+      </h1>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

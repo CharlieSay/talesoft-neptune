cb892f8d482d91ffe436f5e1ebcc81e5013a2c06
Charlie Say <charliewsay@gmail.com>
2023-05-24 19:23:19 +0100

    fixing the mobile index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import type { NextPage } from "next";
-import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
+import { RoughNotation } from "react-rough-notation";
 
 const Home: NextPage = () => {
   return (
-    <section className="px-4 mt-32 md:mt-64">
-      <h1 className="z-50 mb-7 antialiased text-2xl md:text-5xl font-semibold">
+    <section className="m-auto p-auto md:mt-64">
+      <h1 className="z-50 mb-7 antialiased text-md md:text-5xl font-semibold">
         Amplify your message through
       </h1>
       <h1 className="hidden md:block pt-4 text-5xl font-extrabold uppercase">
@@ -17,7 +17,7 @@ const Home: NextPage = () => {
           Digital Transformation
         </RoughNotation>
       </h1>
-      <h1 className="block text-white flex-wrap md:hidden pt-4 text-5xl font-extrabold uppercase">
+      <h1 className="block text-white flex-wrap md:hidden pt-4 text-2xl  font-extrabold uppercase">
         <RoughNotation type="highlight" color="#1d4fd8" show={true}>
           Digital Transformation
         </RoughNotation>

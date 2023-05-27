43aacf6908b239d057e3c980a8cd7200537f0de1
Charlie Say <charliewsay@gmail.com>
2023-05-27 17:51:49 +0100

    hiding mob menu till pages are done

--- components/layout.tsx
+++ components/layout.tsx
@@ -33,13 +33,13 @@ export const Layout = (props: { children: React.ReactNode }) => {
               <button
                 data-collapse-toggle="mobile-menu"
                 type="button"
-                className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                className="hidden inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                 aria-controls="mobile-menu"
                 aria-expanded="false"
               >
                 <span className="sr-only">Open main menu</span>
                 <svg
-                  className="w-6 h-6"
+                  className="w-6 h-6x"
                   fill="currentColor"
                   viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg"
